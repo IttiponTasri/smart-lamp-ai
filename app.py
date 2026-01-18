e57d0641120a521faf90d8effcdf32f9dcc478de
IttiponTasri <i.tasri@anusarn-deaf.ac.th>
2026-01-18 13:51:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,31 @@ import joblib
 import numpy as np
 import os
 
+app = Flask(__name__)
+
+# โหลด model + label encoder
 model, le = joblib.load("lamp_ai_full.pkl")
 
+@app.route("/")
+def home():
+    return "Smart Lamp AI API is running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+
+    X = np.array([[
+        data["zone"],
+        data["current"],
+        data["power"],
+        data["broken"]
+    ]])
+
+    pred = model.predict(X)
+    status = le.inverse_transform(pred)[0]
+
+    return jsonify({"prediction": status})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
