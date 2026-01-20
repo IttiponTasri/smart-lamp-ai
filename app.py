3cd6cec977d4f2be56402839c6eabc41205ba8c3
IttiponTasri <i.tasri@anusarn-deaf.ac.th>
2026-01-20 11:31:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+import os
+
+app = Flask(__name__)   # <<< ต้องอยู่ก่อน @app.route ทุกอัน
+
+# โหลด model + label encoder
+model, le = joblib.load("lamp_ai_full.pkl")
+
+@app.route("/")
+def home():
+    return "Smart Lamp AI API is running"
+
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
     if request.method == "GET":
@@ -20,3 +34,8 @@ def predict():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
